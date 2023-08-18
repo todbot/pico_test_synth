27bdff84961608f55d671a9f873234c26e88ef61
Tod Kurt <todbot@users.noreply.github.com>
2023-08-17 21:47:19 -0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # pico_test_synth
+
+A simple [Raspberry Pi Pico RP2040](https://www.raspberrypi.com/documentation/microcontrollers/raspberry-pi-pico.html)-based
+synth to experiment with [`synthio`](https://github.com/todbot/circuitpython-synthio-tricks).
+
+<img src="./docs/pico_test_synth1.jpg" width=500>
+<img src="./docs/pico_test_synth2.jpg" width=500>
+
+### Features
+
+* [PCM5102 I2S DAC](https://todbot.com/blog/2023/05/16/cheap-stereo-line-out-i2s-dac-for-circuitpython-arduino-synths/) footprint for high-quality stereo audio out
+* Optoisolated MIDI Input via [MIDI TRS-A 3.5mm jack](https://www.perfectcircuit.com/make-noise-0-coast-midi-cable.html)
+* MIDI output via MIDI TRS-A 3.5mm jack
+* Two pots for controlling parameters
+* One switch for controlling parameters
+* 16 capsense touch buttons for synth triggering
+* USB MIDI in/out of course too
+
+### Software
+
+Programs written for [`qtpy_synth`](https://github.com/todbot/qtpy_synth/)
+will work with this board too with minimal changes.
+
+Some programs written specifically for this board:
+
+* [hwtest](https://github.com/todbot/pico_test_synth/tree/main/circuitpython/hwtest/code.py) - test out the hardware with a simple synth
+
+More code specific for this board coming!
+
+#### See also:
+- https://github.com/todbot/circuitpython-synthio-tricks
+  - Contains [many other synthio examples](https://github.com/todbot/circuitpython-synthio-tricks/tree/main/examples) that can work with this synth with minimal changes
+
+
+
+### Pins used
+
+The board uses all of the QTPy RP2040 pins:
+
+* `board.GP26` - left knob
+* `board.GP27` - right knob
+* `board.GP28` - middle button
+* `board.GP17` - TRS UART MIDI in
+* `board.GP18` - TRS UART MIDI out
+* `board.GP20` - I2S bit clock
+* `board.GP21` - I2S LR  clock
+* `board.GP22` - I2S data
+* `board.GP19` & `board.GP18` - I2C bus for OLED display
+* `board.GP0`..`board.GP15` -- touch pads 1-16
+
+For more details see [`hwtest/code.py`](https://github.com/todbot/pico_test_synth/tree/main/circuitpython/hwtest/code.py).
+
+
+### Schematic
+
+[<img src="./docs/pico_test_synth1_sch.png" width=500>](./schematics/pico_test_synth1_sch.pdf)
---
+++ circuitpython/hwtest/code.py
@@ -0,0 +1,172 @@
+# qtpy_synth_hwtest_code.py -- test hardware of qtpy_synth board
+# 27 Jun 2023 - @todbot / Tod Kurt
+#
+# Functionality:
+# - touch pads 1,2,3,4 trigger synth notes (defined in 'midi_notes')
+# - middle button triggers random synth notes
+# - left knob controls filter cutoff
+# - right knob controls filter resonance
+# - sending TRS UART MIDI will print out those bytes to the REPL
+#
+# Libaries needed:
+# - asyncio
+# - adafruit_debouncer
+# - adafruit_displayio_ssd1306
+# - adafruit_display_text
+# Install them all with:
+#   circup install asyncio adafruit_debouncer adafruit_displayio_ssd1306 adafruit_display_text
+#
+#
+import asyncio
+import time, random
+import board, busio
+import analogio, keypad
+import audiopwmio, audiobusio, audiomixer, synthio
+import ulab.numpy as np
+import displayio, terminalio
+import adafruit_displayio_ssd1306
+from adafruit_display_text import bitmap_label as label
+import touchio
+from adafruit_debouncer import Debouncer
+
+#midi_notes = (33, 45, 52, 57)
+midi_notes = list(range(45,45+16))
+filter_freq = 4000
+filter_resonance = 1.2
+
+# pin definitions
+pwm_audio_pin = board.GP22
+sw_pin = board.GP28
+knobB_pin  = board.GP27
+knobA_pin  = board.GP26
+i2s_data_pin = board.GP22
+i2s_lclk_pin = board.GP21
+i2s_bclk_pin = board.GP20
+i2c_scl_pin = board.GP19
+i2c_sda_pin = board.GP18
+uart_rx_pin = board.GP17
+uart_tx_pin = board.GP16
+
+touch_pins = (
+    board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5,
+    board.GP6, board.GP7 ,board.GP8, board.GP9, board.GP10, board.GP11,
+    board.GP12, board.GP13, board.GP14, board.GP15 )
+
+# begin board setup
+displayio.release_displays()
+
+# set up our knobs and button
+knobA = analogio.AnalogIn(knobA_pin)
+knobB = analogio.AnalogIn(knobB_pin)
+keys = keypad.Keys( pins=(sw_pin,),  value_when_pressed=False )
+
+# set up touch pins using a debouncer
+touchins = []
+touchs = []
+for pin in touch_pins:
+    print("touch pin:", pin)
+    touchin = touchio.TouchIn(pin)
+    touchin.threshold = int(touchin.threshold * 1.05)
+    touchins.append(touchin)
+    touchs.append( Debouncer(touchin) )
+
+
+uart = busio.UART(rx=uart_rx_pin, tx=uart_tx_pin, baudrate=31250, timeout=0.001)
+i2c = busio.I2C(scl=i2c_scl_pin, sda=i2c_sda_pin, frequency=1_000_000 )
+
+dw,dh = 128, 64
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3c )
+display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=dw, height=dh, rotation=180)
+
+# set up the synth->audio system
+audio = audiobusio.I2SOut(bit_clock=i2s_bclk_pin, word_select=i2s_lclk_pin, data=i2s_data_pin)
+mixer = audiomixer.Mixer(voice_count=1, sample_rate=28000, channel_count=1,
+                         bits_per_sample=16, samples_signed=True,
+                         buffer_size=8192)  # buffer_size=4096)  # need a big buffer when screen updated
+synth = synthio.Synthesizer(sample_rate=28000)
+audio.play(mixer)
+mixer.voice[0].level = 0.75 # turn down the volume a bit since this can get loud
+mixer.voice[0].play(synth)
+
+# set up the synth
+wave_saw = np.linspace(30000,-30000, num=256, dtype=np.int16)  # default squ is too clippy
+amp_env = synthio.Envelope(sustain_level=0.8, release_time=0.4, attack_time=0.001)
+synth.envelope = amp_env
+
+# set up info to be displayed
+maingroup = displayio.Group()
+display.show(maingroup)
+text1 = label.Label(terminalio.FONT, text="helloworld...", x=0, y=10)
+text2 = label.Label(terminalio.FONT, text="@todbot", x=0, y=25)
+text3 = label.Label(terminalio.FONT, text="hwtest. press!", x=0, y=50)
+for t in (text1, text2, text3):
+    maingroup.append(t)
+
+touch_notes = [None] * len(midi_notes)
+sw_pressed = False
+
+def check_touch():
+    for i in range(len(touchs)):
+        touch = touchs[i]
+        touch.update()
+        if touch.rose:
+            print("touch press",i)
+            f = synthio.midi_to_hz(midi_notes[i])
+            filter = synth.low_pass_filter(filter_freq, filter_resonance)
+            n = synthio.Note( frequency=f, waveform=wave_saw, filter=filter )
+            synth.press( n )
+            touch_notes[i] = n
+        if touch.fell:
+            print("touch release", i)
+            synth.release( touch_notes[i] )
+
+async def debug_printer():
+    while True:
+        text1.text = "K:%3d %3d S:%d" % (knobA.value//255, knobB.value//255, sw_pressed)
+        text2.text = "T:" + ''.join(["%3d " % v for v in (touchins[0].raw_value//16, touchins[1].raw_value//16, touchins[2].raw_value//16, touchins[3].raw_value//16)])
+        print(text1.text)
+        print(text2.text)
+        await asyncio.sleep(0.3)
+
+async def input_handler():
+    global sw_pressed
+    global filter_freq, filter_resonance
+
+    note = None
+
+    while True:
+        filter_freq = knobA.value/65535 * 8000 + 100  # range 100-8100
+        filter_resonance = knobB.value/65535 * 3 + 0.2  # range 0.2-3.2
+
+        for n in touch_notes:  # real-time adjustment of filter
+            if n:
+                n.filter = synth.low_pass_filter(filter_freq, filter_resonance)
+
+        check_touch()
+
+        if key := keys.events.get():
+            if key.released:
+                sw_pressed = False
+                synth.release( note )
+            if key.pressed:
+                sw_pressed = True
+                f = synthio.midi_to_hz(random.randint(32,60))
+                note = synthio.Note(frequency=f, waveform=wave_saw) # , filter=filter)
+                synth.press(note)
+        await asyncio.sleep(0.005)
+
+async def uart_handler():
+    while True:
+        while msg := uart.read(3):
+            print("midi:", [hex(b) for b in msg])
+        await asyncio.sleep(0)
+
+# main coroutine
+async def main():  # Don't forget the async!
+    task1 = asyncio.create_task(debug_printer())
+    task2 = asyncio.create_task(input_handler())
+    task3 = asyncio.create_task(uart_handler())
+    await asyncio.gather(task1,task2,task3)
+
+print("hello qtpy_synth hwtest")
+asyncio.run(main())
