3a7da3bd5e2db96d3c5c73301ec9bcc7a597d9d3
Tod Kurt <todbot@users.noreply.github.com>
2024-07-28 12:29:48 -0700

    add touch keys to monosynth1

--- arduino/monosynth1/monosynth1.ino
+++ arduino/monosynth1/monosynth1.ino
@@ -5,14 +5,15 @@
  *
  * Responds to Serial (DIN) MIDI In and USB MIDI
  *
- *  @todbot 3 Jan 2021
+ * @todbot 3 Jan 2021
  **/
 
 // set DEBUG_MIDI 1 to show CCs received in Serial Monitor
-#define DEBUG_MIDI 0
+#define DEBUG_MIDI 1
 
 #include <Adafruit_TinyUSB.h>
 #include <MIDI.h>
+#include <TouchyTouch.h>
 
 #include "MozziConfigValues.h"  // for named option values
 #define MOZZI_AUDIO_MODE MOZZI_OUTPUT_I2S_DAC
@@ -22,14 +23,14 @@
 #define MOZZI_I2S_PIN_BCK 20
 #define MOZZI_I2S_PIN_WS (MOZZI_I2S_PIN_BCK+1) // HAS TO BE NEXT TO pBCLK, i.e. default is 21
 #define MOZZI_I2S_PIN_DATA 22
-#define OSCIL_DITHER_PHASE 1 
+#define MOZZI_ANALOG_READ MOZZI_ANALOG_READ_NONE  // we're using regular analogRead()
+//#define OSCIL_DITHER_PHASE 1 
 // Mozzi's controller update rate, seems to have issues at 1024
 // If slower than 512 can't get all MIDI from Live
-#define MOZZI_CONTROL_RATE 512 
-// #define MOZZI_CONTROL_RATE 128 // mozzi rate for updateControl()
+#define MOZZI_CONTROL_RATE 512  // mozzi rate for updateControl()
+//#define MOZZI_CONTROL_RATE 128 // mozzi rate for updateControl()
 
 #include <Mozzi.h>
-
 #include <Oscil.h>
 #include <tables/triangle_analogue512_int8.h>
 #include <tables/square_analogue512_int8.h>
@@ -59,11 +60,17 @@ Adafruit_USBD_MIDI usb_midi;                                  // USB MIDI object
 MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDIusb);  // USB MIDI
 // MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDIuart);      // Serial MIDI
 
+const int touch_pins[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+const int touch_count = sizeof(touch_pins) / sizeof(int);
+const int touch_threshold_adjust = 100;
+
+TouchyTouch touches[touch_count];
+bool touched[touch_count];
 
 // SETTINGS
 //int portamento_time = 50;  // milliseconds
 //int env_release_time = 1000; // milliseconds
-byte sound_mode = 0; // patch number / program change
+byte patch_num = 0; // patch number / program change
 bool retrig_lfo = true;
 
 enum KnownCCs {
@@ -94,7 +101,6 @@ typedef struct {
     float filtermod_freq;
     float attack_time;
     float release_time;
-    
 } Patch;
 
 
@@ -115,8 +121,11 @@ ADSR <CONTROL_RATE, MOZZI_AUDIO_RATE> envelope;
 Portamento <MOZZI_CONTROL_RATE> portamento;
 LowPassFilter lpf;
 
-//
-void setup() {
+int notes_on = 0;
+uint32_t last_debug_millis; 
+
+// core0 is UI
+void setup() { 
   pinMode(LED_BUILTIN, OUTPUT);
 
   // Serial1.setRX(uart_rx_pin);
@@ -128,21 +137,47 @@ void setup() {
   // MIDIuart.begin(MIDI_CHANNEL_OMNI); // don't forget OMNI
   // MIDIuart.turnThruOff();  // turn off echo
 
+  // TOUCH
+  for (int i = 0; i < touch_count; i++) {
+    touches[i].begin(touch_pins[i]);
+    touches[i].threshold += touch_threshold_adjust;  // make a bit more noise-proof
+    touched[i] = false;
+  }
+}
+
+// core0 is UI
+void loop() { 
+  handleMIDI();
+  
+  updateInputs();
+
+  if( millis() - last_debug_millis > 100 ) { 
+    last_debug_millis = millis();
+    for (int i = 0; i < touch_count; i++) {
+      char t = touches[i].touched() ? '|' : ' ';  // indicates a touched value
+      int touchval = touches[i].raw_value / 100;  // make a more printalbe value
+      Serial.printf("%c%2d%c", t, touchval, t);
+    }
+    Serial.println();
+  }
+
+}
+
+// core1 is audio
+void setup1() {
   startMozzi(MOZZI_CONTROL_RATE);
-
-  handleProgramChange(0); // set our initial patch
-
+  handleProgramChange(patch_num); // set our initial patch
  }
-
-//
-void loop() {
+ 
+// core1 is audio
+void loop1() {
   audioHook();
 }
 
-int notes_on = 0;
-
 //
 void handleNoteOn(byte channel, byte note, byte velocity) {
+  (void) channel;
+  (void) velocity;
   #if DEBUG_MIDI
   Serial.println("midi_test handleNoteOn!");
   #endif
@@ -154,6 +189,9 @@ void handleNoteOn(byte channel, byte note, byte velocity) {
 
 //
 void handleNoteOff(byte channel, byte note, byte velocity) {
+  (void) channel;
+  (void) note;
+  (void) velocity;
   digitalWrite(LED_BUILTIN,LOW);
   notes_on--;
   if( ! notes_on ) { 
@@ -163,6 +201,7 @@ void handleNoteOff(byte channel, byte note, byte velocity) {
 
 //
 void handleControlChange(byte channel, byte cc_num, byte cc_val) {
+  (void) channel;
   #if DEBUG_MIDI 
   Serial.printf("CC %d %d\n", cc_num, cc_val);
   #endif
@@ -183,14 +222,14 @@ void handleControlChange(byte channel, byte cc_num, byte cc_val) {
 //
 void handleProgramChange(byte m) {
   Serial.print("program change:"); Serial.println((byte)m);
-  sound_mode = m;
-  if( sound_mode == 0 ) {    
+  patch_num = m;
+  if( patch_num == 0 ) {    
     aOsc1.setTable(SAW_ANALOGUE512_DATA);
     aOsc2.setTable(SAW_ANALOGUE512_DATA);
     
     mod_vals[Modulation] = 0;   // FIXME: modulation unused currently
     mod_vals[Resonance] = 93;
-    mod_vals[FilterCutoff] = 60;
+    mod_vals[FilterCutoff] = 50;
     mod_vals[PortamentoTime] = 50; // actually in milliseconds
     mod_vals[EnvReleaseTime] = 100; // in 10x milliseconds (100 = 1000 msecs)
 
@@ -201,7 +240,7 @@ void handleProgramChange(byte m) {
     envelope.setTimes(50, 200, 20000, mod_vals[EnvReleaseTime]*10 );
     portamento.setTime( mod_vals[PortamentoTime] );
   }
-  else if ( sound_mode == 1 ) {
+  else if ( patch_num == 1 ) {
     aOsc1.setTable(SQUARE_ANALOGUE512_DATA);
     aOsc2.setTable(SQUARE_ANALOGUE512_DATA);
     
@@ -215,7 +254,7 @@ void handleProgramChange(byte m) {
     envelope.setTimes(50, 100, 20000, (uint16_t)mod_vals[EnvReleaseTime]*10 );
     portamento.setTime( mod_vals[PortamentoTime] );
   }
-  else if ( sound_mode == 2 ) {
+  else if ( patch_num == 2 ) {
     aOsc1.setTable(TRIANGLE_ANALOGUE512_DATA);
     aOsc2.setTable(TRIANGLE_ANALOGUE512_DATA);
     mod_vals[FilterCutoff] = 65;
@@ -247,16 +286,27 @@ void handleMIDI() {
   }
 }
 
-// mozzi function, called at CONTROL_RATE times per second
+void updateInputs() {
+  static int touchi = 0;
+
+  // button.update();
+  // knobA_update();
+  // knobB_update();
+
+  // only check one touch button per loop since touchytouch spinloops
+  touches[touchi].update();
+  if( touches[touchi].pressed() ) { 
+    handleNoteOn( 0, 36 + touchi, 127);
+  }
+  if( touches[touchi].released() ) { 
+    handleNoteOff( 0, 36 + touchi, 0);
+  }
+  touchi = (touchi+1) % touch_count;
+
+}
+
+// Mozzi function, called at CONTROL_RATE times per second on core1
 void updateControl() {
-  handleMIDI();
-  
-  // map the lpf modulation into the filter range (0-255), corresponds with 0-8191Hz, kFilterMod runs -128-127
-  //uint8_t cutoff_freq = cutoff + (mod_amount * (kFilterMod.next()/2));
-//  uint16_t fm = ((kFilterMod.next() * mod_vals[Modulation]) / 128) + 127 ; 
-//  uint8_t cutoff_freq = constrain(mod_vals[FilterCutoff] + fm, 0,255 );
-  
-//  lpf.setCutoffFreqAndResonance(cutoff_freq, mod_vals[Resonance]*2);
 
   lpf.setCutoffFreqAndResonance(mod_vals[FilterCutoff], mod_vals[Resonance]*2);  // don't *2 filter since we want 0-4096Hz
 
@@ -264,14 +314,13 @@ void updateControl() {
   
   Q16n16 pf = portamento.next();  // Q16n16 is a fixed-point fraction in 32-bits (16bits . 16bits)
   aOsc1.setFreq_Q16n16(pf);
-  //aOsc2.setFreq_Q16n16(pf * 1.02);  // FIXME: adjustable detune plz
-  aOsc2.setFreq_Q16n16(pf * (1 + rand(10)/1000.0));  // FIXME: adjustable detune plz
-
+  aOsc2.setFreq_Q16n16(pf * 1.001);  // FIXME: adjustable detune plz
 }
 
-// mozzi function, called at AUDIO_RATE times per second
+// Mozzi function, called at AUDIO_RATE times per second on core1
 AudioOutput updateAudio() {
-  long asig = lpf.next( aOsc1.next() + aOsc2.next() );
+  int32_t asig = aOsc1.next() + aOsc2.next();
+  asig = lpf.next( asig );
   asig = envelope.next() * asig;
   return StereoOutput::fromAlmostNBit(18, asig, asig); // 16 = 8 signal bits + 8 envelope bits
 }
